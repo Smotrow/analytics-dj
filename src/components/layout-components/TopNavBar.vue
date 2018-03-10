0191064475aa04212ad9544cdca5e80598c84500
Novikov Dima <novikovd1996@gmail.com>
2018-03-10 21:14:22 +0200

    create adaptive menu

--- src/components/layout-components/TopNavBar.vue
+++ src/components/layout-components/TopNavBar.vue
@@ -1,34 +1,60 @@
 <template lang="jade">
   nav.top-nav-bar( role="navigation")
-    .container-fluid
-      .navbar-header
-        a#main-logo.navbar-brand( href="http://domjurista.ua/")
-          img(src='/static/img/logo.png')
-    .container-fluid
-      .navbar-header
-        router-link#sub-logo.navbar-brand( to="main") Analytics
-      .collapse.navbar-collapse
-        ul.nav.navbar-nav.navbar-right
-          li
-            router-link(:to="{ name: 'Stories'}") Історії
-          li
-            router-link(:to="{ name: 'AboutSystem'}") Про систему
-          li
-            router-link(:to="{ name: 'HowItWorks'}") Як працює
-          li
-            router-link(to="#") Контакти
-          li
-            span
-               router-link.general-link.btn.btn-primary.btn-group-xs(to="#") Розпочати безкоштовно
+    .desktop-menu
+      .container-fluid
+        .navbar-header
+          a.main-logo.navbar-brand( href="http://domjurista.ua/")
+            img(src='/static/img/logo.png')
+      .container-fluid
+        .navbar-header
+          router-link.sub-logo.navbar-brand( to="main") Analytics
+        .collapse.navbar-collapse
+          ul.nav.navbar-nav.navbar-right
+            li(v-for="item in menuItems")
+              router-link(:to="{ name: item.name}") {{item.title}}
+            li
+              span
+                router-link.general-link.btn.btn-primary.btn-group-xs(to="#") Розпочати безкоштовно
+    .mobile-menu
+      .container-fluid
+        .navbar-header
+          router-link.sub-logo.navbar-brand( to="main") Analytics
+          a.toggle-menu.pull-right(@click="toggleMenu")
+            i(v-show="!showMenu")
+              i.fas.fa-bars
+            i(v-show="showMenu")
+              i.fas.fa-times
+        .collapse-menu( v-if="showMenu")
+          ul.nav
+            li(v-for="item in menuItems", @click="closeMenu")
+              router-link(:to="{ name: item.name}") {{item.title}}
+            li
+              span
+                router-link.general-link.btn.btn-primary.btn-group-xs(to="#") Розпочати безкоштовно
 </template>
 
 <script>
   export default {
     name: 'top-nav-bar',
     data () {
-      return {}
+      return {
+        menuItems: [
+          {name: 'Stories', title: 'Історії'},
+          {name: 'AboutSystem', title: 'Про систему'},
+          {name: 'HowItWorks', title: 'Як працює'},
+          {name: 'Main', title: 'Контакти'}
+        ],
+        showMenu: false
+      }
     },
-    methods: {},
+    methods: {
+      toggleMenu () {
+        this.showMenu = !this.showMenu
+      },
+      closeMenu () {
+        this.showMenu = false
+      }
+    },
     created () {}
   }
 </script>
