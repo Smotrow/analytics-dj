90afc82afb8dc8538b7584143a6106258abb38e9
Novikov Dima <novikovd1996@gmail.com>
2018-03-12 01:32:29 +0200

    created slider

---
+++ src/components/sliders/SliderFullScreen.vue
@@ -0,0 +1,64 @@
+<template lang="jade">
+  .slider-full
+    .slide-arrow.arrow-previous(@click="previousSlide")
+      a.arrow
+        i.fas.fa-chevron-circle-left
+    .slide-arrow.arrow-next(@click="nextSlide")
+      a.arrow
+        i.fas.fa-chevron-circle-right
+    .slider-container(:style="slideImg(slide.bgImg)")
+      .slider-header
+        p.none {{title}}
+      .slider-content.container-global
+        .content-title
+          p.none.weight-6 {{slide.title}}
+        .content-text
+          p.none {{slide.text}}
+      .slider-navigation
+        a.navigation-btn(
+        v-for="nav in slides",
+        @click="showSlide(nav.id)",
+        :class="nav.id === slide.id ? 'active-slide' : ''") {{nav.title}}
+</template>
+
+<script>
+  import _ from 'lodash'
+
+  export default {
+    name: 'slider-full-screen',
+    props: {
+      title: String,
+      slides: {
+        type: Array
+      }
+    },
+    data () {
+      return {
+        slide: null
+      }
+    },
+    computed: {},
+    created () {
+      this.slide = this.slides[0]
+    },
+    methods: {
+      slideImg (bg) {
+        return bg ? 'background-image: ' + bg : ''
+      },
+      showSlide (slideId) {
+        this.slide = _.find(this.slides, {id: slideId})
+      },
+      nextSlide () {
+        let nextIndex = this.slide.id === (this.slides.length) ? 1 : (this.slide.id + 1)
+        this.showSlide(this.slides[nextIndex - 1].id)
+      },
+      previousSlide () {
+        let previousIndex = this.slide.id === 0 ? (this.slides.length) : (this.slide.id - 1)
+        this.showSlide(this.slides[previousIndex - 1].id)
+      }
+    }
+  }
+</script>
+
+<style lang="scss">
+</style>
--- src/pages/AboutSystemPage.vue
+++ src/pages/AboutSystemPage.vue
@@ -1,51 +1,45 @@
 <template lang="jade">
   div
     header
-      hero-board(:bgImg="heroBg")
+      hero-board.about-system-hero(:bgImg="heroBg", :needSubstrate="false")
         div(slot="header")
-          h3 Ми не даємо відповіді на запитання.
-            font.text-primary  Ми відшукуємо справедливість.
+          h3.weight-3 Ми не даємо відповіді на запитання.
         div(slot="content")
-          p.none Перший в
-            font.text-primary  Україні
+          p.none Перший в Україні
             |  сервіс на основі обчислювального інтелекту, який прогнозує результати рішень судових спорів.
     main
-      section.info-section.container-global
-        .row
-          .col-xs-12.col-sm-9.col-md-7
-            p.none.weight-6 {{textMore}}
-      demo-stories(:stories="demoStories")
+      section.slider-full-screen
+        slider-full-screen(:slides="fullSlides", :title="'About Analytics'")
 </template>
 
 <script>
   import HeroBoard from '../components/HeroBoard.vue'
-  import DemoStories from '../components/DemoStories.vue'
+  import SliderFullScreen from '../components/sliders/SliderFullScreen.vue'
 
   export default {
     components: {
       [HeroBoard.name]: HeroBoard,
-      [DemoStories.name]: DemoStories
+      [SliderFullScreen.name]: SliderFullScreen
     },
     data () {
       return {
-        heroBg: 'url("/static/img/stories-hero.jpg")',
-        demoStories: [
+        heroBg: 'url("/static/img/about-hero.jpg")',
+        fullSlides: [
           {
             id: 1,
+            title: 'Test1',
             text: 'Як працює Analytics?',
             bgImg: 'url("/static/img/asset-story-1.jpg")'
           }, {
             id: 2,
+            title: 'Test2',
             text: 'Історії користувачів',
             bgImg: 'url("/static/img/asset-story-2.jpg")'
           }, {
             id: 3,
+            title: 'Test3',
             text: 'Про систему Analytics',
             bgImg: 'url("/static/img/asset-story-3.jpg")'
-          }, {
-            id: 4,
-            text: 'Про систему Analytics',
-            bgImg: 'url("/static/img/asset-story-4.jpg")'
           }
         ],
         textMore: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. ' +
