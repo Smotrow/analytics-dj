6c6fdff546a9e164791b25bee993afb822bf7aa3
Novikov Dima <novikovd1996@gmail.com>
2018-03-26 00:29:58 +0300

    created steps logic

---
+++ src/mixins/steps-data.js
@@ -0,0 +1,21 @@
+export default {
+  data () {
+    return {
+      steps: [
+        {
+          id: 1,
+          name: 'Test1',
+          text: 'Як працює Analytics?'
+        }, {
+          id: 2,
+          name: 'Test2',
+          text: 'Історії користувачів'
+        }, {
+          id: 3,
+          name: 'Test3',
+          text: 'Про систему Analytics'
+        }
+      ]
+    }
+  }
+}
--- src/pages/WorkPanel.vue
+++ src/pages/WorkPanel.vue
@@ -10,10 +10,10 @@
             .col-xs-12.title-block
               .header-wrap
                 .step-name
-                  h2.text-dark-grey.none name
+                  h2.text-dark-grey.none {{step.name}}
                 .step-number
-                  h3.text-dark-grey.none 1 3
-        #step-1.step
+                  h3.text-dark-grey.none Крок {{step.id}} з 3
+        #step-1.steps(v-show="currentStep === 1")
           .row
             .col-md-4.info-block
               .row.text-small
@@ -61,9 +61,8 @@
                   .col-xs-4
                     p email
                   .col-xs-4
-                    p email
-                  .col-xs-4
-                    p email
+                    p пездюк
+                  .col
           .row
             .col-md-4.col-xs-12
               .category
@@ -76,14 +75,17 @@
               .options
                 radio-input(:modelObject="stepFirst", typeIndex="option", value="2", id="premium2")
                 label email
-        #step-2.step
+          .row
+            .col-xs-12.text-right
+              a.next.btn.btn-primary-bright(@click="nextStep") Далі
+        #step-2.steps(v-show="currentStep === 2")
           .row
             .col-sm-push-6.col-sm-5.col-sm-offset-1.col-xs-12.animation-block
               p.header.weight-6 {{textLess}}
               .animation
                 //TODO: animation
                 img(src="/static/img/animation-img.png")
-            .col-sm-pull-6.col-sm-6.col-xs-12.questions-block(v-for="question in questions", v-if="question.id === currentId")
+            .col-sm-pull-6.col-sm-6.col-xs-12.questions-block(v-for="question in questions", v-if="question.id === currentQuestion")
               .row.question-wrap
                 .col-xs-12.header
                   p.weight-6 {{textLess}}
@@ -92,13 +94,13 @@
               .row.answer-wrap
                 .col-xs-12.header
                   p.weight-6 {{textLess}}
-                .col-xs-12.answer(v-for="answer in question.answers")
+                .col-xs-12.answer(v-for="answer in question.answers", @click="nextQuestion(question.question, answer)")
                   i.check
                   p {{answer}}
               .row.back-wrap
                 .col-xs-12.back
-                  p.weight-6 Back
-        #step-3.step
+                  p.weight-6(@click="previousStep") Back
+        #step-3.steps(v-show="currentStep === 3")
           .row
             .col-md-6.col-xs-12
               .result
@@ -115,11 +117,14 @@
               .premium-result
                 .result-header
                   p.weight-6 {{textLess}}
-                .result-content.text-small
-                  .opportunities
-                    p.weight-4 {{textLess}}
-                  .cost
-                    p.weight-6 Cost - 1000
+                .opportunities.text-small
+                  i.more-info
+                  p.weight-4 {{textLess}}
+                .opportunities.text-small
+                  i.variantes
+                  p.weight-4 {{textLess}}
+                .cost
+                  p.weight-6 Cost - 1000
                 .result-footer
                   .send-block
                     p.weight-6 Send
@@ -129,11 +134,13 @@
 
 <script>
   import RadioInput from '../components/inputs/RadioInput.vue'
+  import StepsData from '../mixins/steps-data'
 
   export default {
     components: {
       [RadioInput.name]: RadioInput
     },
+    mixins: [StepsData],
     data () {
       return {
         test: false,
@@ -146,10 +153,20 @@
           rate: '',
           option: ''
         },
-        currentId: 0,
+        step: {},
+        currentStep: 1,
+        currentQuestion: 0,
         questions: [
           {
             id: 0,
+            question: 'Lorem ipsum dolor sit amet. ',
+            answers: [
+              'Lorem ipsum dolor sit amet. ',
+              'Lorem ipsum dolor sit amet. ',
+              'Lorem ipsum dolor sit amet. '
+            ]
+          }, {
+            id: 1,
             question: 'Lorem ipsum dolor sit amet. ',
             answers: [
               'Lorem ipsum dolor sit amet. ',
@@ -166,9 +183,25 @@
         'Diam, eu fringilla enim nunc id dui. Suspendisse a luctus leo.',
         textLess: 'Lorem ipsum dolor sit amet. '
       }
+    },
+    created () {
+      this.step = this.steps[0]
+    },
+    methods: {
+      nextStep () {
+        ++this.currentStep
+        this.step = this.steps[this.currentStep - 1]
+      },
+      nextQuestion (question, answer) {
+        if (this.currentQuestion + 1 === this.questions.length) this.nextStep()
+        ++this.currentQuestion
+        console.log(question)
+        console.log(answer)
+      },
+      previousStep () {
+        --this.currentStep
+        this.step = this.steps[this.currentStep - 1]
+      }
     }
   }
 </script>
-
-<style lang="scss">
-</style>
