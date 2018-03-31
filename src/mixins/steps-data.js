2b43b6fdc2cbe6e99954a6a81d92b80b525bf4a0
Novikov Dima <novikovd1996@gmail.com>
2018-04-01 00:31:28 +0300

    create animation

--- src/mixins/steps-data.js
+++ src/mixins/steps-data.js
@@ -1,6 +1,7 @@
 export default {
   data () {
     return {
+      step: {},
       steps: [
         {
           id: 1,
@@ -14,6 +15,91 @@ export default {
           id: 3,
           name: 'Test3',
           text: 'Про систему Analytics'
+        }
+      ],
+      currentStep: 1,
+      currentQuestion: 0,
+      questions: [
+        {
+          id: 0,
+          question: 'Lorem ipsum dolor sit amet. ',
+          answers: [
+            'Lorem ipsum dolor sit amet. ',
+            'Lorem ipsum dolor sit amet. ',
+            'Lorem ipsum dolor sit amet. '
+          ]
+        }, {
+          id: 1,
+          question: 'Lorem ipsum dolor sit amet. ',
+          answers: [
+            'sdsds. ',
+            'dsar sit amet. ',
+            'Loremsdsit amet. '
+          ]
+        }, {
+          id: 2,
+          question: 'Lorem ipsum dolor sit amet. ',
+          answers: [
+            'fdsfsfsd. ',
+            'asdsa. ',
+            'dsdst. '
+          ]
+        }, {
+          id: 3,
+          question: 'Lorem ipsum dolor sit amet. ',
+          answers: [
+            'fdsfsfsd. ',
+            'asdsa. ',
+            'dsdst. '
+          ]
+        }, {
+          id: 4,
+          question: 'Lorem ipsum dolor sit amet. ',
+          answers: [
+            'fdsfsfsd. ',
+            'asdsa. ',
+            'dsdst. '
+          ]
+        }, {
+          id: 5,
+          question: 'Lorem ipsum dolor sit amet. ',
+          answers: [
+            'fdsfsfsd. ',
+            'asdsa. ',
+            'dsdst. '
+          ]
+        }, {
+          id: 6,
+          question: 'Lorem ipsum dolor sit amet. ',
+          answers: [
+            'fdsfsfsd. ',
+            'asdsa. ',
+            'dsdst. '
+          ]
+        }, {
+          id: 7,
+          question: 'Lorem ipsum dolor sit amet. ',
+          answers: [
+            'fdsfsfsd. ',
+            'asdsa. ',
+            'dsdst. '
+          ]
+        }, {
+          id: 8,
+          question: 'Lorem ipsum dolor sit amet. ',
+          answers: [
+            'fdsfsfsd. ',
+            'asdsa. ',
+            'dsdst. '
+          ]
+        }, {
+          id: 9,
+          question: 'Lorem ipsum dolor sit amet. ',
+          answers: [
+            'fdsfsfsd. ',
+            'asdsa. ',
+            'dsdst. '
+          ]
         }
       ]
     }
--- src/pages/WorkPanel.vue
+++ src/pages/WorkPanel.vue
@@ -13,137 +13,147 @@
                   h2.text-dark-grey.none {{step.name}}
                 .step-number
                   h3.text-dark-grey.none Крок {{step.id}} з 3
-        #step-1.steps(v-show="currentStep === 1")
-          .row
-            .col-md-4.info-block
-              .row.text-small
-                .col-xs-12.info-text
-                  p {{textMore}}
+        transition-group(name="fade")
+          #step-1.steps(v-show="currentStep === 1", :key="1")
+            .row
+              .col-md-4.info-block
+                .row.text-small
+                  .col-xs-12.info-text
+                    p {{textMore}}
+                  .col-xs-12
+                    .info-footer
+                      .button-block
+                        span.btn-purple Обчислювальний інтелект
+                      .supported-block
+                        p.text-purple.supported Supported by
+              .col-md-8
+                .row
+                  .col-xs-12
+                    label email
+                    input.classic(v-model="stepFirst.email")
+                //.row
+                  .col-md-4.col-xs-12
+                    label email
+                    input.classic(v-model="stepFirst.email")
+                  .col-md-4.col-xs-12
+                    label email
+                    input.classic(v-model="stepFirst.email")
+                  .col-md-4.col-xs-12
+                    label email
+                    input.classic(v-model="stepFirst.email")
+                .alternative.text-center.text-small
+                  .row.header.vertical-align
+                    .col-xs-4
+                      label email
+                    .col-xs-4
+                      label email
+                    .col-xs-4
+                      label email
+                  .row.rate.vertical-align
+                    radio-input(:modelObject="stepFirst", typeIndex="rate", value="standard", id="standard")
+                    .col-xs-4
+                      p email
+                    .col-xs-4
+                      p email
+                    .col-xs-4
+                      p email
+                  .row.rate.vertical-align.premium(data-special="Найкраща пропозиція")
+                    radio-input(:modelObject="stepFirst", typeIndex="rate", value="premium", id="premium")
+                    .col-xs-4
+                      p email
+                    .col-xs-4
+                      p email
+                    .col-xs-4
+                      p email
+            .row
+              .col-md-4.col-xs-12
+                .category
+                  h3.none {{textLess}}
+              .col-md-4.col-xs-12
+                .options
+                  radio-input(:modelObject="stepFirst", typeIndex="rate", value="1", id="premium1")
+                  label email
+              .col-md-4.col-xs-12
+                .options
+                  radio-input(:modelObject="stepFirst", typeIndex="rate", value="2", id="premium2")
+                  label email
+            .row
+              .col-xs-12.text-right
+                a.next.btn.btn-primary-bright(@click="nextStep") Далі
+          #step-2.steps(v-show="currentStep === 2", :key="2")
+            .row
+              .col-sm-push-6.col-sm-5.col-sm-offset-1.col-xs-12.animation-block
                 .col-xs-12
-                  .info-footer
-                    .button-block
-                      span.btn-purple Обчислювальний інтелект
-                    .supported-block
-                      p.text-purple.supported Supported by
-            .col-md-8
-              .row
-                .col-xs-12
-                  label email
-                  input.classic(v-model="stepFirst.email")
-              //.row
-                .col-md-4.col-xs-12
-                  label email
-                  input.classic(v-model="stepFirst.email")
-                .col-md-4.col-xs-12
-                  label email
-                  input.classic(v-model="stepFirst.email")
-                .col-md-4.col-xs-12
-                  label email
-                  input.classic(v-model="stepFirst.email")
-              .alternative.text-center.text-small
-                .row.header.vertical-align
-                  .col-xs-4
-                    label email
-                  .col-xs-4
-                    label email
-                  .col-xs-4
-                    label email
-                .row.rate.vertical-align
-                  radio-input(:modelObject="stepFirst", typeIndex="rate", value="standard", id="standard")
-                  .col-xs-4
-                    p email
-                  .col-xs-4
-                    p email
-                  .col-xs-4
-                    p email
-                .row.rate.vertical-align.premium(data-special="Найкраща пропозиція")
-                  radio-input(:modelObject="stepFirst", typeIndex="rate", value="premium", id="premium")
-                  .col-xs-4
-                    p email
-                  .col-xs-4
-                    p email
-                  .col-xs-4
-                    p email
-          .row
-            .col-md-4.col-xs-12
-              .category
-                h3.none {{textLess}}
-            .col-md-4.col-xs-12
-              .options
-                radio-input(:modelObject="stepFirst", typeIndex="rate", value="1", id="premium1")
-                label email
-            .col-md-4.col-xs-12
-              .options
-                radio-input(:modelObject="stepFirst", typeIndex="rate", value="2", id="premium2")
-                label email
-          .row
-            .col-xs-12.text-right
-              a.next.btn.btn-primary-bright(@click="nextStep") Далі
-        #step-2.steps(v-show="currentStep === 2")
-          .row
-            .col-sm-push-6.col-sm-5.col-sm-offset-1.col-xs-12.animation-block
-              p.header.weight-6 {{textLess}}
-              .animation
-                div(:class="animationClass")
-                  .element(v-for="n in 20", :id="'el_'+n")
-                  .line(v-for="n in 10", :id="'ln_'+n")
-            .col-sm-pull-6.col-sm-6.col-xs-12.questions-block(v-for="question in questions", v-if="question.id === currentQuestion")
-              .row.question-wrap
-                .col-xs-12.header
-                  p.weight-6 {{textLess}}
-                .col-xs-12.question
-                  p {{question.question}}
-              .row.answer-wrap
-                .col-xs-12.header
-                  p.weight-6 {{textLess}}
-                .col-xs-12.answer(v-for="answer in question.answers", @click="nextQuestion(question.question, answer)")
-                  i.check
-                  p {{answer}}
-              .row.back-wrap
-                .col-xs-12.back
-                  p.weight-6(@click="previousStep") Back
-        #step-3.steps(v-show="currentStep === 3 && !isPaid")
-          .row
-            .col-md-6.col-xs-12
-              .result
-                i.logo
-                .result-header
-                  p.weight-6 {{textLess}}
-                .result-content.text-small
-                  p.weight-4  {{textLess}}
-                .result-footer
-                  .send-block
-                    p.weight-6 Send
-                    .send
-            .col-md-6.col-xs-12
-              .premium-result
-                .result-header
-                  p.weight-6 {{textLess}}
-                .opportunities.text-small
-                  i.more-info
-                  p.weight-4 {{textLess}}
-                .opportunities.text-small
-                  i.variants
-                  p.weight-4 {{textLess}}
-                .cost
-                  p.weight-6 Cost - 1000
-                .result-footer
-                  .send-block(@click="payment")
-                    p.weight-6 Send
-                    i.send
-        #step-after-payment.steps(v-show="currentStep === 3 && isPaid")
-          .row
-            .col-sm-8.col-sm-offset-2.col-xs-12
-              .result
-                i.logo
-                .result-header
-                  p.weight-6 {{textLess}}
-                .result-content.text-small
-                  p.weight-4  {{textLess}}
-                .result-footer
-                  .send-block
-                    p.weight-6 Send
-                    i.send
+                  p.header.weight-6 {{textLess}}
+                .animation(@click="refresh")
+                  div(:class="animationClass")
+                    .element(v-for="n in 20", :id="'el_'+n")
+                    .line(v-for="n in 10", :id="'ln_'+n")
+              .col-sm-pull-6.col-sm-6.col-xs-12.questions-block(v-for="(question,index) in questions", v-if="question.id === currentQuestion")
+                transition-group(name="fade")
+                  .row.question-wrap(v-if="!isEnd", :key="'question'")
+                    .col-xs-12.header
+                      p.weight-6 {{textLess}}
+                    .col-xs-12.question
+                      p {{index+' '+question.question}}
+                  .row.answer-wrap(v-if="!isEnd", :key="'answer'")
+                    .col-xs-12.header
+                      p.weight-6 {{textLess}}
+                    .col-xs-12.answer(v-for="answer in question.answers", @click="nextQuestion(question.question, answer)")
+                      i.check
+                      p {{answer}}
+                  .row.question-wrap(v-if="isEnd", :key="'result'")
+                    .col-xs-12.header
+                      p.weight-6 {{textLess}}
+                    .col-xs-12.question
+                      p {{index+' '+question.question}}
+                .row.back-wrap
+                  .col-xs-12.back
+                    transition(name="fade")
+                      p.weight-6(@click="previousStep" v-if="!isEnd") Back
+                      p.weight-6(@click="nextStep" v-else=) Back
+          #step-3.steps(v-show="currentStep === 3 && !isPaid", :key="3")
+            .row
+              .col-md-6.col-xs-12
+                .result
+                  i.logo
+                  .result-header
+                    p.weight-6 {{textLess}}
+                  .result-content.text-small
+                    p.weight-4  {{textLess}}
+                  .result-footer
+                    .send-block
+                      p.weight-6 Send
+                      .send
+              .col-md-6.col-xs-12
+                .premium-result
+                  .result-header
+                    p.weight-6 {{textLess}}
+                  .opportunities.text-small
+                    i.more-info
+                    p.weight-4 {{textLess}}
+                  .opportunities.text-small
+                    i.variants
+                    p.weight-4 {{textLess}}
+                  .cost
+                    p.weight-6 Cost - 1000
+                  .result-footer
+                    .send-block(@click="payment")
+                      p.weight-6 Send
+                      i.send
+          #step-after-payment.steps(v-show="currentStep === 3 && isPaid", :key="4")
+            .row
+              .col-sm-8.col-sm-offset-2.col-xs-12
+                .result
+                  i.logo
+                  .result-header
+                    p.weight-6 {{textLess}}
+                  .result-content.text-small
+                    p.weight-4  {{textLess}}
+                  .result-footer
+                    .send-block
+                      p.weight-6 Send
+                      i.send
 </template>
 
 <script>
@@ -158,8 +168,6 @@
     data () {
       return {
         isPaid: false,
-        test: false,
-        test1: false,
         stepFirst: {
           email: '',
           lastName: '',
@@ -168,37 +176,8 @@
           rate: '',
           option: ''
         },
-        step: {},
+        isEnd: false,
         animationClass: 'start',
-        currentStep: 1,
-        currentQuestion: 0,
-        questions: [
-          {
-            id: 0,
-            question: 'Lorem ipsum dolor sit amet. ',
-            answers: [
-              'Lorem ipsum dolor sit amet. ',
-              'Lorem ipsum dolor sit amet. ',
-              'Lorem ipsum dolor sit amet. '
-            ]
-          }, {
-            id: 1,
-            question: 'Lorem ipsum dolor sit amet. ',
-            answers: [
-              'sdsds. ',
-              'dsar sit amet. ',
-              'Loremsdsit amet. '
-            ]
-          }, {
-            id: 2,
-            question: 'Lorem ipsum dolor sit amet. ',
-            answers: [
-              'fdsfsfsd. ',
-              'asdsa. ',
-              'dsdst. '
-            ]
-          }
-        ],
         textMore: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. ' +
         'Diam, eu fringilla enim nunc id dui. Suspendisse a luctus leo.' +
         'Diam, eu fringilla enim nunc id dui. Suspendisse a luctus leo.' +
@@ -212,6 +191,10 @@
       this.step = this.steps[0]
     },
     methods: {
+      refresh () {
+        this.currentQuestion = 0
+        this.animationClass = 'start'
+      },
       nextStep () {
         ++this.currentStep
         this.step = this.steps[this.currentStep - 1]
